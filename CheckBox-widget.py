1dc5ff7d50b96f435ffe9dd51bfe7f2c62f19607
AditiChandraK <147531833+AditiChandraK@users.noreply.github.com>
2023-11-21 18:09:33 +0530

    Add files via upload

--- CheckBox-widget.py
+++ CheckBox-widget.py
@@ -1,10 +1,10 @@
 import tkinter
 from tkinter import*
 root=tkinter.Tk()
-root.title=("BABY JUST SAY YES")
+root.title=("LOVE STORY- TS")
 root.geometry=("1500x1500")
 root.configure(bg='#121212')
-w=Label(root, text ='Do you love me?', foreground="Black",background="Red",font = "100",width=100) 
+w=Label(root, text ='ARE YOU JULIET?', foreground="Black",background="Red",font = "100",width=100) 
 w.pack() 
 
 Checkbutton1 = IntVar() # holds integer data passed to the checkbutton widget
@@ -12,8 +12,8 @@ Checkbutton2 = IntVar()
 Checkbutton3 = IntVar()
 
 cb1=Checkbutton(root, text="yes",variable = Checkbutton1, onvalue = 1, offvalue = 0, height = 2, width = 100,background="green",foreground="white")
-cb2=Checkbutton(root, text = "YES, MARRY ME", variable = Checkbutton2, onvalue = 1, offvalue = 0, height = 2, width = 100,background="pink",foreground="white")
-cb3=Checkbutton(root, text = "NO BISH GO DIE", variable = Checkbutton3, onvalue = 1, offvalue =0, height = 2, width = 100,background="Purple",foreground="white") 
+cb2=Checkbutton(root, text = "YES, BUT DID YOU ASK MY DAD", variable = Checkbutton2, onvalue = 1, offvalue = 0, height = 2, width = 100,background="pink",foreground="white")
+cb3=Checkbutton(root, text = "SO CAN GO PICK OUT A WHITE DRESS", variable = Checkbutton3, onvalue = 1, offvalue =0, height = 2, width = 100,background="Purple",foreground="white") 
 cb1.pack() 
 cb2.pack() 
 cb3.pack()
